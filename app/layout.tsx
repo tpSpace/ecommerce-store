57f52a5c964f0af3207c87a8a28830606be2efe8
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-06-22 01:12:49 +0700

    Rename modal && add layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,33 @@
-import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
-import "./globals.css";
-import Footer from "@/components/footer";
+
+import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+import "./globals.css";
 
 const font = Urbanist({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Store",
-  description: "The Store",
+  description: "Store - The place for all your purchases.",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <Navbar />
-      <body className={font.className}>{children}</body>
-      <Footer />
+      <body className={font.className}>
+        <ToastProvider />
+        <ModalProvider />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
